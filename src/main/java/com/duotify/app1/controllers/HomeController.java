a0bc1281f511cfc7d36a87416715bacc98522404
Will <doggy.huang@gmail.com>
2022-09-14 00:52:36 +0800

    Initial commit

---
+++ src/main/java/com/duotify/app1/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.duotify.app1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HomeController
+ */
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    public HomeController() {
+    }
+
+    @GetMapping
+    public String getString() {
+        return "Hello World";
+    }
+}
